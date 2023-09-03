65d0df1e6fc95b0012a6eca10af63c6659a59c3c
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-09-03 04:14:42 -0300

    updated hello world to use express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+import express from "express"
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send("Hello world!")
+})
+
+app.listen(80, () => {
+	console.log("App listening on port 80")
+})
