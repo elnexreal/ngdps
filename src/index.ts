a354305175f1544afc504ad9d52c3444e95debc7
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-09-03 05:16:26 -0300

    added bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express"
 import prisma from "./lib/db/prismaClient"
+import bodyParser from "body-parser"
 import "dotenv/config"
 const app = express()
+
+app.use(
+	bodyParser.urlencoded({ extended: true })
+)
 
 app.get("/", (req, res) => {
 	res.send("Hello world!")
