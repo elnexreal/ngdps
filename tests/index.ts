3e7dc3f201d406dda058ca164879c4e001d8390e
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-09-03 04:50:02 -0300

    updated test

--- tests/index.ts
+++ tests/index.ts
@@ -2,12 +2,13 @@ import express from "express"
 import prisma from "../src/lib/db/prismaClient"
 const app = express()
 
+prisma.$connect()
+
 app.use("/", (req, res) => {
 	res.send("Hello world!")
 })
 
 app.listen(8080, () => {
 	console.log("SUCCESS")
+	process.exit()
 })
-
-prisma.$connect()
