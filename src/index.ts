4c80cc33cd5ba67603db0ddcb9bbc2d719a603e2
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-09-03 04:46:03 -0300

    added `PORT` in `.env`

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import prisma from "./lib/db/prismaClient"
+import "dotenv/config"
 const app = express()
 
 app.get("/", (req, res) => {
 	res.send("Hello world!")
 })
 
-app.listen(80, () => {
+app.listen(process.env.PORT, () => {
 	console.log("App listening on port 80")
 })
